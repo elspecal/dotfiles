f6cadb09fb13396de1cac6af4ad430fc6602333e
vimonuri <vimonuri@x200.arch.dev>
2021-09-07 13:09:09 +0200

    Hide file tree on startup.

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -2,4 +2,4 @@ local map = vim.api.nvim_set_keymap
 
 map('!', 'kj', '<Esc>', { noremap = true })
 map('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true})
-require 'nvim-tree'.toggle()
+-- require 'nvim-tree'.toggle()
